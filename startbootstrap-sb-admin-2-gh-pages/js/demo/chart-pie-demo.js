d30e1dd86080b6f1fd713b577381570acdd80c1c
Phil <znzpzp13@naver.com>
2021-12-21 23:19:15 +0900

    12.19 api 화면 출력 시도중 객체 안으로 키값으로 접근을 못하고있음... 계속 시도중 - 출력완료! 12.21 파일 합치기 성공!그래프에 넣기 성공!!!좀 더 가다듬자

--- startbootstrap-sb-admin-2-gh-pages/js/demo/chart-pie-demo.js
+++ startbootstrap-sb-admin-2-gh-pages/js/demo/chart-pie-demo.js
@@ -33,29 +33,3 @@ var myPieChart = new Chart(ctx, {
     cutoutPercentage: 80,
   },
 });
-<script
-    src="https://code.jquery.com/jquery-3.6.0.min.js"
-    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-    crossOrigin="anonymous"></script>
-
-
-  let apiArr = []
-
-  document.querySelector('#btn').addEventListener('click', () => {
-  $.getJSON('https://api.odcloud.kr/api/15071595/v1/uddi:262b7fb0-1f5e-40cd-8f14-b0614b4dc997?page=1&perPage=10&serviceKey=Fs0FMSahbQEYkW4qd0bKDnX6ualVgtwD6luFAYuMaOG8c4P%2B%2Bhsjn%2BMh2qNQZoG9AFPrTvZFq%2FJ013lo8dUBWA%3D%3D', function (source){
-    apiArr = source.data
-    // console.log(apiArr)
-    const ulEle = document.querySelector('#showApi')
-    let str = ''
-    console.log(apiArr)
-    // let sortKeys = Object.keys(apiArr).filter(function (sort){return sort.indexOf('구분') >-1 })
-    // console.log(sortKeys[0])
-    const value = Object.keys(apiArr)
-
-    for (let i = 0; i < apiArr.length; i++) {
-      str += `<ul>출력확인 : ${apiArr[i].구분}</ul>
-                        <div>수익률 : ${apiArr[i]["2017년 수익률"]}</div>`
-    }//end for
-    ulEle.innerHTML = str
-  })
-})
