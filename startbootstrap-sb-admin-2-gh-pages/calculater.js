b43ab6943b968741549950ca7cbc52da0e8e5d1e
Phil <znzpzp13@naver.com>
2021-12-25 00:47:53 +0900

    12.19 api 화면 출력 시도중 객체 안으로 키값으로 접근을 못하고있음... 계속 시도중 - 출력완료! 12.21 파일 합치기 성공!그래프에 넣기 성공!!!좀 더 가다듬자 들어올 데이터에 대해서 제대로 살펴보지 않았다. 일단 구분 카테고리는 늘어나지 않으니 수동으로 입력하고 마우스를 위에 올렸을 때 나타나도록 해야겠다. 연도별 수익률이랑 수익금 역산해서 투자원금 계산해보자 일단 2017년 데이터만 상세하게 그래프에 뿌려보자. 12.22 일단 동작하게 만들고 이후 리팩토링을 하자 for 문 돌릴 곳이 보인다. 숫자로 변환완료했지만 계속 써줘야하는 거 같아서 함수를 손보자 변환완료했다. 금액이 퍼센트로 치환해줘야겠다. 일단 총 금액을 출력 후 각자 금액에 대해서 퍼센트로 치환하는 함수 만들어야겠다. 12.23 구분키로 접근하는 데이터에 대해서 순서대로 출력해주는 함수 만들고 있음 즉 배열 반환 함수 구분은 완료했는데 Data에 넣을 수 있는 타입이 아님 안에서 배열 함수를 순서대로 반환해야 할 것 같음 12.24 일단 도는 거랑 심플하게 만드는 거랑 같이 만드는중 함수만드는중..parameter값의 개수에 맞지않게 argument를 넘겨버버리면 형변환이 제대로 되지않는 걸 알게되었다

--- startbootstrap-sb-admin-2-gh-pages/calculater.js
+++ startbootstrap-sb-admin-2-gh-pages/calculater.js
@@ -1,8 +1,22 @@
-function calculater(ratioPercent, profit) {
-    const toHundred = 100/ratioPercent
-    return profit*toHundred;
+// function calculater(ratioPercent, profit) {
+//     const toHundred = 100/ratioPercent
+//     return profit*toHundred;
+// }
+//
+// console.log(calculater(7.28, 41190))
+
+function monenytoPercent(data1, data2, data3, data4) {
+    const money = parseFloat(data1) + parseFloat(data2) + parseFloat(data3) + parseFloat(data4);
+    const preresult1 = money/parseFloat(data1)
+    // const preresult2 = money/data2
+    // const preresult3 = money/data3
+    // const preresult4 = money/data4
+    const percent = 100
+    const result = percent/parseFloat(preresult1)
+    console.log(result)
+
+
 }
+monenytoPercent(20,20, 20, 20)
 
-console.log(calculater(7.28, 41190))
-
-2
+// prarameter 값에 맞지않게 argument를 넘겨버리면 에러가 발생하는 걸 알게 되었다
