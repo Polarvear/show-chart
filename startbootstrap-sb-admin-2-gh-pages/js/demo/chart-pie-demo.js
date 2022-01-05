de502a119aec6a5473c199392860b2e92cdc606e
Hello <znzpzp13@naver.com>
2022-01-05 20:45:10 +0900

    home

--- startbootstrap-sb-admin-2-gh-pages/js/demo/chart-pie-demo.js
+++ startbootstrap-sb-admin-2-gh-pages/js/demo/chart-pie-demo.js
@@ -2,8 +2,8 @@
 const pie = []
 fetch('https://api.odcloud.kr/api/15071595/v1/uddi:262b7fb0-1f5e-40cd-8f14-b0614b4dc997?page=1&perPage=10&serviceKey=Fs0FMSahbQEYkW4qd0bKDnX6ualVgtwD6luFAYuMaOG8c4P%2B%2Bhsjn%2BMh2qNQZoG9AFPrTvZFq%2FJ013lo8dUBWA%3D%3D')
     .then(response => response.json())
-    .then(data => {
-        const year = data.data // 출력확인
+    .then(result => {
+        const year = result.data // 출력확인
         year.map(m => pie.push(m["2017년 수익금(단위: 십억원)"]))
     })
 console.log(pie)
@@ -15,9 +15,9 @@ var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
     type: 'doughnut',
     data: {
-        labels: ["Direct", "Referral", "Social"],
+        labels: pie,
         datasets: [{
-            data: [50, 50, 0],
+            data: [50, 101, 1023, 114,141,4,1424,41414,4141],
             backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
             hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
             hoverBorderColor: "rgba(234, 236, 244, 1)",
